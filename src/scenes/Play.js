16df9aadc2e41e3f9b838ec55e662cd4da52ac2a
Jonah Ryan <jopryan@ucsc.edu>
2023-05-18 23:05:07 -0700

    Hammer Kind of works

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,44 +7,54 @@ class Play extends Phaser.Scene{
         this.load.setPath("./assets/");
         this.load.image('spr_hammer','hammer.png');
         this.load.image('ball','spr_ball.png');
-        
+        this.load.image('ground','ground.png');
+        this.load.image('hit_1','hitboxBig.png');
+        this.load.image('hit_2','hitboxSmall.png')
+
         this.input.enabled = true;
     }
 
     create(){
         
-                this.matter.world.setBounds();
-        
-                //  Our two bodies which will be connected by a constraint (aka a Joint or a Spring)
-        
-                const ballA = this.matter.add.image(420, 0, 'spr_hammer', null, { shape: 'rectangle', friction: 0.005, restitution: 0.6});
-                const ballB = this.matter.add.image(400, 400, 'ball', null, { shape: 'rectangle', friction: 0.005, restitution: 0.6 });
-        
-                //  You can create a constraint between the two bodies using a Factory function.
-                //  The value 100 is the resting length and 0.2 is the stiffness of the constraint.
-        
-                //this.matter.add.constraint(ballA, ballB, 100, 0.2);
-        
-                //  To help those of you more used to the Box2D syntax you can use
-                //  add.joint or add.spring instead (with the exact same parameters)
-        
-                this.matter.add.joint(ballA, ballB, 100, 0.2);
-                this.matter.add.joint(ballA, ballB, 100, 0.2);
-        
-                //  Or you can create a native Matter constraint:
-        
-                // var constraint = Phaser.Physics.Matter.Matter.Constraint.create({
-                //     bodyA: ballA.body,
-                //     bodyB: ballB.body,
-                //     length: 100,
-                //     stiffness: 0.2
-                // });
-        
-                //  Which you then have to add to the world yourself:
-        
-                // this.matter.world.add(constraint);
-        
-                this.matter.add.mouseSpring();
-            }
+        this.matter.world.setBounds();
+
+        const group1 = this.matter.world.nextCategory();
+
+        const group2 = this.matter.world.nextCategory();
+        const group3 = this.matter.world.nextCategory();
+
+        //  Our two bodies which will be connected by a constraint (aka a Joint or a Spring)
+        this.ballA = this.matter.add.image(400, 0, 'hit_1', null, { 
+            shape: 'rectangle', friction: 0.005, restitution: 0.6
+         }).setVisible(false);
+
+        this.ballB = this.matter.add.image(400, 400, 'hit_2', null, { 
+            shape: 'rectangle', friction: 0.005, restitution: 0.6
+         }).setVisible(false);
+
+        this.ground = this.matter.add.image(640/2, 480, 'ground', null, { 
+            shape: 'rectangle',  isStatic:true
+         });
+
+
+        this.hammer = new Hammer(this,420,0,'spr_hammer');
+
+        this.matter.add.joint(this.ballA, this.ballB, 64, 0.2);
+
+        //  Or you can create a native Matter constraint:
+        this.matter.add.mouseSpring({
+            length: 0.01,
+            stiffness: 1,
+            angularStiffness: 1,
+        });
+
+        }
+
+    update(){
+        this.hammer.x = this.ballA.x;
+        this.hammer.y = this.ballA.y;
+        this.hammer.angle = Phaser.Math.RadToDeg( Phaser.Math.Angle.Between(this.ballA.x,this.ballA.y,this.ballB.x,this.ballB.y)) - 90
+    }
 }
+
 
