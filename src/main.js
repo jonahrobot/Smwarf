737d4940b19976b0ef0e0e87741ac49ad60f72b0
Sinfinite01 <54284225+Sinfinite01@users.noreply.github.com>
2023-05-25 19:15:45 -0700

    debug off

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ let config = {
     physics: {
       default: 'matter',
       matter: {
-        debug: true
+        debug: false //true
       }
     },
     scene: [ Menu, Tutorial, Play, Leaderboard ]
